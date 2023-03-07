3cedfeb25af3742ee24e2ce9629508007a29da2f
ceddddy <76970719+ceddddy@users.noreply.github.com>
2023-03-07 19:52:51 +0200

    added event emmiter

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
-@Component({
-  selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
+@Component({// component definition
+  selector: 'app-product-alerts',//IdentifiesTheComponent
+  templateUrl: './product-alerts.component.html',//references the component's HTML and CSS(template and style filenames)
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent {
-  @Input() product!: Product;
-  
+
+// pass data to a child component
+export class ProductAlertsComponent {//handles functionality for the component
+  @Input() product!: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
